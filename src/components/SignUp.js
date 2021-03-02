9810522653750874976262a107c0ab5a18611c51
Dhia Eddine Nini <dxn00@mail.aub.edu>
2021-03-03 00:35:45 +0100

    Added support for REST API. Reacts app now interacts with the backend + backend saves guests into database

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -9,6 +9,16 @@ class SignUp extends Component {
   state = {
     showForm: false,
     isClient: false,
+    guestFullName: "",
+    guestPhoneNumber: "",
+  };
+
+  handleNameChange = (event) => {
+    this.setState({ guestFullName: event.target.value });
+  };
+
+  handlePhoneNumberChange = (event) => {
+    this.setState({ guestPhoneNumber: event.target.value });
   };
 
   formatRadioButtons() {
@@ -78,18 +88,25 @@ class SignUp extends Component {
     } else {
       return (
         <div style={{ textAlign: "center" }}>
-          <input class="secondary-input" type="name" placeholder="Full Name" />
+          <input
+            class="secondary-input"
+            type="name"
+            placeholder="Full Name"
+            onChange={this.handleNameChange}
+          />
           <br />
           <input
             class="secondary-input"
             type="tel"
             placeholder="Phone Number"
+            onChange={this.handlePhoneNumberChange}
           />
           <br />
           <br />
           <MyButton
             class="rounded-btn secondary-btn"
             value="Sign Up"
+            onClick={this.handleGuestSignUp}
           ></MyButton>
         </div>
       );
@@ -123,6 +140,21 @@ class SignUp extends Component {
 
   handleGuestClick = () => {
     this.setState({ showForm: true, isClient: false });
+  };
+
+  handleGuestSignUp = () => {
+    // create POST request with Guest JSON:
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        name: this.state.guestFullName,
+        phoneNumber: this.state.guestPhoneNumber,
+      }),
+    };
+
+    // send request to sign up:
+    fetch("http://127.0.0.1:5000/auth/register_guest", requestOptions);
   };
 
   render() {
