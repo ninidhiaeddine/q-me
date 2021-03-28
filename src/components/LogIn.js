3d9fa4ce8c1780e81e2e4ac3203681fd8ab22243
Dhia Eddine Nini <dxn00@mail.aub.edu>
2021-03-28 20:26:23 +0100

    React now sends HTTP requests of sign up and login (no redirection so far)

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -7,47 +7,67 @@ import MyNavBar from "./MyNavBar";
 class LogIn extends Component {
   state = {
     showForm: false,
-    isHost: false,
+    isEstablishment: false,
+
+    guestForm: {
+      name: "",
+      phoneNumber: "",
+    },
+
+    establishmentForm: {
+      email: "",
+      password: "",
+    },
   };
 
   formatForm() {
-    if (this.state.isHost) {
+    if (this.state.isEstablishment) {
       return (
         <div style={{ textAlign: "center" }}>
           <input
             class="secondary-input"
             type="email"
             placeholder="Email Address"
+            onChange={this.handleEstablishmentEmailChange}
           />
           <br />
           <input
             class="secondary-input"
             type="password"
             placeholder="Password"
+            onChange={this.handleEstablishmentPasswordChange}
           />
           <br />
           <br />
           <MyButton
             class="rounded-btn primary-btn"
-            value="Sign In"
+            value="Sign In as Establishment"
+            onClick={this.handleEstablishmentSignIn}
           ></MyButton>
         </div>
       );
     } else {
       return (
         <div style={{ textAlign: "center" }}>
-          <input class="secondary-input" type="name" placeholder="Full Name" />
+          <input
+            class="secondary-input"
+            type="name"
+            placeholder="Full Name"
+            onChange={this.handleGuestNameChange}
+          />
           <br />
           <input
             class="secondary-input"
             type="tel"
             placeholder="Phone Number"
+            onChange={this.handleGuestPhoneNumberChange}
           />
           <br />
           <br />
           <MyButton
             class="rounded-btn secondary-btn"
-            value="Sign In"
+            value="Sign In as Guest"
+            onClick={this.handleGuestSignIn}
           ></MyButton>
         </div>
       );
@@ -59,8 +79,8 @@ class LogIn extends Component {
       <div style={{ textAlign: "center" }}>
         <MyButton
           class="rounded-btn primary-btn"
-          value="Log In as Host"
-          onClick={this.handleClientClick}
+          value="Log In as Establishment"
+          onClick={this.handleEstablishmentClick}
         />
         <br />
         <MyButton
@@ -72,12 +92,75 @@ class LogIn extends Component {
     );
   }
 
-  handleClientClick = () => {
-    this.setState({ showForm: true, isHost: true });
+  handleGuestNameChange = (e) => {
+    var guestForm = this.state.guestForm;
+    guestForm.name = e.target.value;
+    this.setState({ guestForm });
+  };
+
+  handleGuestPhoneNumberChange = (e) => {
+    var guestForm = this.state.guestForm;
+    guestForm.phoneNumber = e.target.value;
+    this.setState({ guestForm });
+  };
+
+  handleEstablishmentEmailChange = (e) => {
+    var establishmentForm = this.state.establishmentForm;
+    establishmentForm.email = e.target.value;
+    this.setState({ establishmentForm });
+  };
+
+  handleEstablishmentPasswordChange = (e) => {
+    var establishmentForm = this.state.establishmentForm;
+    establishmentForm.password = e.target.value;
+    this.setState({ establishmentForm });
+  };
+
+  handleGuestSignIn = () => {
+    var data = {
+      name: this.state.guestForm.name,
+      phone_number: this.state.guestForm.phoneNumber,
+    };
+    console.log(JSON.stringify(data));
+    // Simple POST request with a JSON body using fetch
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    };
+    fetch("http://127.0.0.1:5000/auth/guests", requestOptions)
+      .then((response) => console.log(response.json()))
+      .then((data) => console.log(data));
+  };
+
+  handleEstablishmentSignIn = () => {
+    var data = {
+      email: this.state.establishmentForm.email,
+      password: this.state.establishmentForm.password,
+    };
+    console.log(JSON.stringify(data));
+
+    // Simple POST request with a JSON body using fetch
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    };
+    fetch("http://127.0.0.1:5000/auth/establishments", requestOptions)
+      .then((response) => console.log(response.json()))
+      .then((data) => console.log(data));
+  };
+
+  handleEstablishmentClick = () => {
+    this.setState({ showForm: true, isEstablishment: true });
   };
 
   handleGuestClick = () => {
-    this.setState({ showForm: true, isHost: false });
+    this.setState({ showForm: true, isEstablishment: false });
   };
 
   render() {
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,22 +1,29 @@
 import React, { Component } from "react";
 import MyButton from "./MyButton";
 import MyLogo from "./MyLogo";
-import Form from "react-bootstrap/Form";
 import MyNavBar from "./MyNavBar";
 import "./my-inputs.css";
 import "./my-switches.css";
 import "./my-dropdown.css";
-import $ from 'jquery';
-
 
 class SignUp extends Component {
-
-
-
-
   state = {
     showForm: false,
-    isHost: false,
+    isEstablishment: false,
+
+    guestForm: {
+      name: "",
+      phoneNumber: "",
+    },
+
+    establishmentForm: {
+      name: "",
+      type: 0,
+      email: "",
+      password: "",
+      confirmedPassowrd: "",
+      phoneNumber: "",
+    },
   };
 
   // formatRadioButtons() {
@@ -42,47 +49,145 @@ class SignUp extends Component {
     document.getElementById("myDropdown").classList.toggle("show");
   }
 
-  hostPost() {
-    var settings = {
-      "url": "q-me.azurewebsites.net/establishments",
-      "method": "POST",
-      "timeout": 0,
-      "headers": {
-        "Content-Type": "application/json",
-        "Cookie": "ARRAffinity=bffb580df78d7165f60f8296e74f37e8d5a5ef15cfd49e51bf0d3e3f75c7c66d"
-      },
-      "data": JSON.stringify({
-        "name": "classic",
-        "type": 0,
-        "email": "classic@hotmail.com",
-        "password": "classic123"
-      }),
+  handleEsablishmentSignUp = () => {
+    var password = this.state.establishmentForm.password;
+    var confirmedPassowrd = this.state.establishmentForm.confirmedPassowrd;
+
+    var data =
+      this.state.establishmentForm.phoneNumber == ""
+        ? {
+            name: this.state.establishmentForm.name,
+            type: this.state.establishmentForm.type,
+            email: this.state.establishmentForm.email,
+            password: password,
+          }
+        : {
+            name: this.state.establishmentForm.name,
+            type: this.state.establishmentForm.type,
+            email: this.state.establishmentForm.email,
+            password: password,
+            phone_number: this.state.establishmentForm.phoneNumber,
+          };
+
+    if (password == confirmedPassowrd) {
+      // Simple POST request with a JSON body using fetch
+      const requestOptions = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      };
+      fetch("http://127.0.0.1:5000/establishments", requestOptions)
+        .then((response) => console.log(response.json()))
+        .then((data) => console.log(data));
+    } else {
+      console.log("ERROR: Passwords are not matching!");
+    }
+  };
+
+  handleGuestSignUp = () => {
+    var data = {
+      name: this.state.guestForm.name,
+      phone_number: this.state.guestForm.phoneNumber,
     };
 
-    $.ajax(settings).done(function (response) {
-      console.log(response);
-    });
-  }
+    // Simple POST request with a JSON body using fetch
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    };
+    fetch("http://127.0.0.1:5000/guests", requestOptions)
+      .then((response) => console.log(response.json()))
+      .then((data) => console.log(data));
+  };
 
   // Close the dropdown if the user clicks outside of it
   formatDropDown() {
     window.onclick = function (event) {
-      if (!event.target.matches('.dropbtn')) {
+      if (!event.target.matches(".dropbtn")) {
         var dropdowns = document.getElementsByClassName("dropdown-content");
         var i;
         for (i = 0; i < dropdowns.length; i++) {
           var openDropdown = dropdowns[i];
-          if (openDropdown.classList.contains('show')) {
-            openDropdown.classList.remove('show');
+          if (openDropdown.classList.contains("show")) {
+            openDropdown.classList.remove("show");
           }
         }
       }
-    }
+    };
   }
 
-  formatForm() {
+  handleGuestNameChange = (e) => {
+    var guestForm = this.state.guestForm;
+    guestForm.name = e.target.value;
+    this.setState({ guestForm });
+  };
 
-    if (this.state.isHost) {
+  handleGuestPhoneNumberChange = (e) => {
+    var guestForm = this.state.guestForm;
+    guestForm.phoneNumber = e.target.value;
+    this.setState({ guestForm });
+  };
+
+  handleEstablishmentNameChange = (e) => {
+    var establishmentForm = this.state.establishmentForm;
+    establishmentForm.name = e.target.value;
+    this.setState({ establishmentForm });
+  };
+
+  handleEstablishmentEmailChange = (e) => {
+    var establishmentForm = this.state.establishmentForm;
+    establishmentForm.email = e.target.value;
+    this.setState({ establishmentForm });
+  };
+
+  handleEstablishmentPasswordChange = (e) => {
+    var establishmentForm = this.state.establishmentForm;
+    establishmentForm.password = e.target.value;
+    this.setState({ establishmentForm });
+  };
+
+  handleEstablishmentConfirmedPasswordChange = (e) => {
+    var establishmentForm = this.state.establishmentForm;
+    establishmentForm.confirmedPassowrd = e.target.value;
+    this.setState({ establishmentForm });
+  };
+
+  handleEstablishmentPhoneNumberChange = (e) => {
+    var establishmentForm = this.state.establishmentForm;
+    establishmentForm.phoneNumber = e.target.value;
+    this.setState({ establishmentForm });
+  };
+
+  handleEstablishmentEstablishmentTypeChange = (e) => {
+    var establishmentForm = this.state.establishmentForm;
+    switch (e.target.value) {
+      // TODO: Remove hardcoding from this part:
+      case "Restaurant":
+        establishmentForm.type = 0;
+        break;
+
+      case "Hotel":
+        establishmentForm.type = 1;
+        break;
+
+      case "Bank":
+        establishmentForm.type = 2;
+        break;
+
+      case "Supermarket":
+        establishmentForm.type = 3;
+        break;
+    }
+    this.setState({ establishmentForm });
+  };
+
+  formatForm() {
+    if (this.state.isEstablishment) {
       return (
         <div style={{ textAlign: "center" }}>
           <br />
@@ -92,6 +197,7 @@ class SignUp extends Component {
             name="name"
             id="name"
             placeholder="Name of Establishment"
+            onChange={this.handleEstablishmentNameChange}
           />
           <br />
           <input
@@ -100,6 +206,7 @@ class SignUp extends Component {
             name="email"
             id="email"
             placeholder="Email Address"
+            onChange={this.handleEstablishmentEmailChange}
           />
           <br />
           <input
@@ -108,19 +215,34 @@ class SignUp extends Component {
             name="password"
             id="password"
             placeholder="New Password"
+            onChange={this.handleEstablishmentPasswordChange}
           />
           <br />
-          {/* <input
+          <input
             class="secondary-input"
             type="password"
             placeholder="Confirm New Password"
-          /> */}
-
+            onChange={this.handleEstablishmentConfirmedPasswordChange}
+          />
+          <br />
+          <input
+            class="secondary-input"
+            type="tel"
+            placeholder="Phone Number (Optional)"
+            onChange={this.handleEstablishmentPhoneNumberChange}
+          />
+          <br />
           <br />
           <div>
-            <label class="etype" for="establishments">Establishment Type:</label>
-
-            <select name="establishments" id="establishments" class="search_categories">
+            <select
+              name="establishments"
+              id="establishments"
+              class="search_categories"
+              onChange={this.handleEstablishmentEstablishmentTypeChange}
+            >
+              <option value="" selected>
+                Establishment Type
+              </option>
               <option value="Restaurant">Restaurant</option>
               <option value="Hotel">Hotel</option>
               <option value="Bank">Bank</option>
@@ -130,31 +252,35 @@ class SignUp extends Component {
           <br />
           <MyButton
             class="rounded-btn secondary-btn"
-            value="Sign Up"
-            onClick={SignUp.hostPost}
+            value="Sign Up as an Establishment"
+            onClick={this.handleEsablishmentSignUp}
             id="epost"
           ></MyButton>
         </div>
       );
-    }
-    
-    else {
+    } else {
       return (
         <div style={{ textAlign: "center" }}>
-          <input class="secondary-input" type="name" placeholder="Full Name" />
+          <input
+            class="secondary-input"
+            type="name"
+            placeholder="Full Name"
+            onChange={this.handleGuestNameChange}
+          />
           <br />
           <input
             class="secondary-input"
             type="tel"
             placeholder="Phone Number"
+            onChange={this.handleGuestPhoneNumberChange}
           />
           <br />
           <br />
           <MyButton
             class="rounded-btn primary-btn"
-            value="Sign Up"
+            value="Sign Up as a Guest"
+            onClick={this.handleGuestSignUp}
           ></MyButton>
-          
         </div>
       );
     }
@@ -168,26 +294,25 @@ class SignUp extends Component {
         <br />
         <MyButton
           class="rounded-btn secondary-btn-gradient"
-          value="Sign Up as Host"
-          onClick={this.handleHostClick}
+          value="Register as Establishment"
+          onClick={this.handleEstablishmentClick}
         />
         <br />
         <MyButton
           class="rounded-btn secondary-btn-gradient"
-          value="Sign Up as Guest"
+          value="Register as Guest"
           onClick={this.handleGuestClick}
         />
       </div>
     );
   }
 
-  
-  handleHostClick = () => {
-    this.setState({ showForm: true, isHost: true });
+  handleEstablishmentClick = () => {
+    this.setState({ showForm: true, isEstablishment: true });
   };
 
   handleGuestClick = () => {
-    this.setState({ showForm: true, isHost: false });
+    this.setState({ showForm: true, isEstablishment: false });
   };
 
   render() {
