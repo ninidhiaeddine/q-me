949f0f77a456f10acd4e2d7c6a43b8c409403cd5
Mahdi Akhatib <mfa66@mail.aub.edu>
2021-04-16 15:19:50 +0300

    modified guest homescreen and the add branch functionality

--- src/pages/EBranches.js
+++ src/pages/EBranches.js
@@ -8,6 +8,7 @@ import "../components/popup.css";
 import "../components/my-input.css";
 import Collapsible from 'react-collapsible';
 import CollapsibleBox from '../components/CollapsibleBox';
+import Typography from '@material-ui/core/Typography';
 class EBranches extends Component {
 
   state = {};
@@ -18,10 +19,84 @@ class EBranches extends Component {
       message: "",
       items: [],
       isOpen: false,
-      setIsOpen: false
+      setIsOpen: false,
+
+      branchForm: {
+        name: "",
+        email: "",
+        password: "",
+        confirmedPassowrd: "",
+        phoneNumber: ""
+      }
+
     };
 
   }
+
+  handlebranchSignUp = () => {
+    var password = this.state.branchForm.password;
+    var confirmedPassowrd = this.state.branchForm.confirmedPassowrd;
+
+    var data =
+      this.state.branchForm.phoneNumber == ""
+        ? {
+          name: this.state.branchForm.name,
+          email: this.state.branchForm.email,
+          password: password,
+        }
+        : {
+          name: this.state.branchForm.name,
+          email: this.state.branchForm.email,
+          password: password,
+          phone_number: this.state.branchForm.phoneNumber,
+        };
+
+    if (password == confirmedPassowrd) {
+      // Simple POST request with a JSON body using fetch
+      const requestOptions = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      };
+      fetch("http://127.0.0.1:5000/branch", requestOptions)
+        .then((response) => console.log(response.json()))
+        .then((data) => console.log(data));
+    } else {
+      console.log("ERROR: Passwords are not matching!");
+    }
+  };
+
+  handlebranchNameChange = (e) => {
+    var branchForm = this.state.branchForm;
+    branchForm.name = e.target.value;
+    this.setState({ branchForm });
+  };
+
+  handlebranchEmailChange = (e) => {
+    var branchForm = this.state.branchForm;
+    branchForm.email = e.target.value;
+    this.setState({ branchForm });
+  };
+
+  handlebranchPasswordChange = (e) => {
+    var branchForm = this.state.branchForm;
+    branchForm.password = e.target.value;
+    this.setState({ branchForm });
+  };
+
+  handlebranchConfirmedPasswordChange = (e) => {
+    var branchForm = this.state.branchForm;
+    branchForm.confirmedPassowrd = e.target.value;
+    this.setState({ branchForm });
+  };
+
+  handlebranchPhoneNumberChange = (e) => {
+    var branchForm = this.state.branchForm;
+    branchForm.phoneNumber = e.target.value;
+    this.setState({ branchForm });
+  };
 
   togglePopup = () => {
     this.setState({ isOpen: !this.state.isOpen });
@@ -71,6 +146,10 @@ class EBranches extends Component {
         <tr key={"item-" + i}>
           <td>
             <CollapsibleBox
+              name= {context.state.branchForm.name}
+              email= {context.state.branchForm.email}
+              password= {context.state.branchForm.password}
+              phone= {context.state.branchForm.phoneNumber}
               type="text"
               value={o}
               onChange={context.handleItemChanged.bind(context, i)}
@@ -104,6 +183,8 @@ class EBranches extends Component {
               value="Add Branch"
               onClick={this.togglePopup}
             />
+            <br />
+            <br />
           </div>
           {isOpen && <Popup
             content={<>
@@ -114,28 +195,28 @@ class EBranches extends Component {
                 class="secondary-input black-input-color mx-2 mt-3"
                 type="name"
                 placeholder="Branch Name"
-                onChange={this.handleGuestNameChange}
+                onChange={this.handlebranchNameChange}
               />
               <br />
               <input
                 class="secondary-input black-input-color mx-2"
                 type="email"
                 placeholder="Branch Email"
-                onChange={this.handleEstablishmentEmailChange}
+                onChange={this.handlebranchEmailChange}
               />
               <br />
               <input
                 class="secondary-input black-input-color mx-2"
                 type="tel"
                 placeholder="Phone Number"
-                onChange={this.handleGuestPhoneNumberChange}
+                onChange={this.handlebranchPhoneNumberChange}
               />
               <br />
               <input
                 class="secondary-input black-input-color mx-2"
                 type="password"
                 placeholder="Password"
-                onChange={this.handleEstablishmentPasswordChange}
+                onChange={this.handlebranchPasswordChange}
               />
               <MyButton
                 class="circular-btn primary-btn-inverse addBranchButton"
--- src/pages/Guest.js
+++ src/pages/Guest.js
@@ -1,18 +1,42 @@
 import React, { Component } from "react";
 import GNavBar from "../components/GNavBar";
+import MyButton from "../components/MyButton";
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import IconButton from '@material-ui/core/IconButton';
+import Button from '@material-ui/core/Button';
+import CameraAltOutlinedIcon from '@material-ui/icons/CameraAltOutlined';
+import { Icon } from "@material-ui/core";
+import "../components/guest.css"
+// import Card from '@material-ui/core/Card';
+// import CardHeader from '@material-ui/core/CardHeader';
+
 class Guest extends Component {
   state = {};
   render() {
     return (
-      <div style={{ textAlign: "center" }}>
+      <div>
         <GNavBar />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <h1>Guest home</h1>
+
+        <div style={{ textAlign: "center" }}>
+          <br />
+          <h1 class="hi">Hi, Guest. Welcome Back!</h1>
+          <br />
+          <Button
+            class="rounded-btn primary-btn-gradient scan-btn"
+            value="Scan QR Code"
+            endIcon={<CameraAltOutlinedIcon />}>
+            Scan QR Code
+        </Button>
+          <br />
+          <br />
+          <h5 class="desc">This will open your device's camera</h5>
+          <h5 class="desc">to scan the QR code</h5>
+          <hr class="rounded divider"></hr>
+          <br />
+          <h1 class="hi">Live Tracking</h1>
+          <br />
+          <br />
+        </div>
       </div>
     );
   }
